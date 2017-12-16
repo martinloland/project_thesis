15105f159b1a23056aea827bfb3caadf1e8acd61
Martin Loeland <martinloland@gmail.com>
2017-12-17 00:56:41 +0100

    matlab script

---
+++ matlab/results.m
@@ -0,0 +1,28 @@
+clc, clear, close
+folder = '../../robot_control/output/';
+files = dir(strcat(folder,'*.csv'));
+plot_var = 5;
+hold on
+legends = {};
+for file = files'
+    index = strfind(file.name, '.');    
+    ms = str2num(file.name(1:index-1));
+    if ((ms >= 1100 && ms < 5000) || ms==15000)
+        import = importdata(strcat(folder,file.name));
+        percentage = import.data(:,1);
+        force = import.data(:,plot_var);
+        plot(percentage,force)
+        legends(end+1) = {strcat(num2str(ms),' ms')};
+    end
+end
+
+import = importdata(strcat(folder,"15000.csv"));
+percentage = import.data(:,1);
+force = import.data(:,plot_var);
+plot(percentage,force)
+legends(end+1) = {'no dynamic force'};
+legend(legends,'Location','southwest')
+title('Joint 1 force magnitude')
+xlabel('Animation percentage [%]')
+ylabel('Joint force [N]')
+saveas(gcf,'plots/joint_force_comp.png')
