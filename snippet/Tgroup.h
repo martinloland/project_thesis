a5851f80db223bfc2e1fad6c523b393b72909049
Martin Loeland <martinloland@gmail.com>
2017-12-18 12:18:35 +0100

    trying to fix references

--- snippet/Tgroup.h
+++ snippet/Tgroup.h
@@ -1,8 +1,8 @@
 class Tgroup
 {
-	IDMthVec gravityCentre_Tg;			// Holds the centre of gravity?
+	IDMthVec gravityCentre_Tg;			// Centre of gravity
 	ExtBasis current_basis;     		// Pos & orientation of group
-	std::vector <ExtBasis> future_basis // What is saved here?
-	std::vector <ExtBasis> past_basis;  // The last basis?
-	std::list<IDMthVec*> coordsPtrs;	// List of points for geometry?
+	std::vector <ExtBasis> future_bases // Planned
+	std::vector <ExtBasis> past_bases;  // Past
+	std::list<IDMthVec*> coordsPtrs;	// Points in geometry
 }
